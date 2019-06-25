335d6f98264685af39a18bb9c59e36b8833f4cc5
VulpineReynard <milare8@gmail.com>
2019-06-25 14:08:13 -0400

    wip: accordion - functional, needs styling and text

--- JavaScript/accordion.js
+++ JavaScript/accordion.js
@@ -1,9 +1,8 @@
 let items = document.querySelectorAll('.accordion a');
-console.log(items);
 
 function toggleAccordion(){
   this.classList.toggle('active');
-  this.nextElementSibling.classList.toggle('active');
+  this.nextElementSibling.nextElementSibling.classList.toggle('active');
 }
 
 items.forEach(item => item.addEventListener('click', toggleAccordion));
