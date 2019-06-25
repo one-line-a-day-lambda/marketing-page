bb270a09ee97f33f9debca64679b1290c0720b2b
VulpineReynard <milare8@gmail.com>
2019-06-25 13:00:25 -0400

    wip: accordion - almost have functionality

---
+++ JavaScript/accordion.js
@@ -0,0 +1,9 @@
+let items = document.querySelectorAll('.accordion a');
+console.log(items);
+
+function toggleAccordion(){
+  this.classList.toggle('active');
+  this.nextElementSibling.classList.toggle('active');
+}
+
+items.forEach(item => item.addEventListener('click', toggleAccordion));
